c254a7c80cbfebecda2de63aec3af26ddb8f98ef
EGA-SUPREMO <cxiesestro@gmail.com>
2020-04-25 12:13:55 -0400

    Fix bugs made on purpose(features) for the sake
    of learning, nobody gonna read this anyways

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,25 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use App\Http\Requests;
+//use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-	public function __construct($request)
+	public function __construct()
 	{
-		$this ->  middleware('Second');
+		$this ->  middleware('second');
 	}
 
-	public function showPath($request)
+	public function showPath(Request $request)
 	{
 		echo "<br>";
 		$uri = $request -> path();
-		echo $uri;
+		echo "URI: ".$uri;
 
 		echo "<br>";
 		$url = $request -> url();
-		echo $url;
+		echo "URL: ".$url;
 
-		echo "<br> $request -> method()";
+		echo "<br>Method: $request -> method()";
 
 	}
 }
