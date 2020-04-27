cd903fe4dbd629699a16c49063ab7db53684718d
EGA-SUPREMO <cxiesestro@gmail.com>
2020-04-27 15:50:32 -0400

    add logic to ProductController

--- Pusher/routes/web.php
+++ Pusher/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/product', 'ProductController');
+Route::resource('/products', 'ProductController');
 
 Route::get('/', function () {
     return view('welcome');
