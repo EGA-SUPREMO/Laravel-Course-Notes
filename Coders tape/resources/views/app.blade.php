88a3a1f818e2b9718810f46519bc958e4b04144d
EGA-SUPREMO <cxiesestro@gmail.com>
2020-05-05 14:24:10 -0400

    add navegation menu

--- Coders tape/resources/views/app.blade.php
+++ Coders tape/resources/views/app.blade.php
@@ -64,6 +64,10 @@
         </style>
     </head>
     <body>
+        <ul>
+            <li><a href="/about-us">aBout Us</a></li>
+            <li><a href="/services">Services</a></li>
+        </ul>
         @yield('content')
     </body>
     </html>
