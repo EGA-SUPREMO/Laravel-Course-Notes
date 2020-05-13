31360ec9dd1a0dd444e3aaf6a2523814868932aa
EGA-SUPREMO <cxiesestro@gmail.com>
2020-05-13 17:30:12 -0400

    Add lazy-loading to show questions
    Signed-off-by: EGA-SUPREMO <cxiesestro@gmail.com>

--- Coders tape/resources/views/questionnaire/show.blade.php
+++ Coders tape/resources/views/questionnaire/show.blade.php
@@ -8,9 +8,22 @@
                 <div class="card-header">{{ $questionnaire -> title }}</div>
 
                 <div class="card-body">
-                	<a href="{{ $questionnaire -> id }}/questions/create" class="btn btn-dark">Create a new question</a>
+                    <a href="{{ $questionnaire -> id }}/questions/create" class="btn btn-dark">Create a new question</a>
                 </div>
             </div>
+            @foreach($questionnaire -> questions as $question)
+                <div class="card mt-4">
+                    <div class="card-header">{{ $question -> question }}</div>
+
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach($question -> answers as $answer)
+                                <li class="list-group-item">{{ $answer -> answer }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
