be653be9b0e80b32d3dd9438cdd5bb3bc8b0a00d
EGA-SUPREMO <cxiesestro@gmail.com>
2020-05-29 12:23:04 -0400

    Fix a seeder creates extra unwanted models
    Signed-off-by: EGA-SUPREMO <cxiesestro@gmail.com>

--- Coders tape/Beginner 7/database/seeds/QuestionnaireSeeder.php
+++ Coders tape/Beginner 7/database/seeds/QuestionnaireSeeder.php
@@ -21,8 +21,12 @@ class QuestionnaireSeeder extends Seeder
                 $question->answers() -> saveMany(factory(Answer::class, 4)->make());
             });
         });*/
-        factory(Questionnaire::class, 2) -> create() -> each(function($questionnaire) {
-            $questionnaire -> questions() -> save(factory(Question::class) -> make());
+/*        factory(Questionnaire::class, 2) -> create() -> each(function ($questionnaire) {
+            $questionnaire -> questions() -> createMany(factory(Question::class, 4) -> make() -> toArray());
+        });*/
+        factory(Questionnaire::class, 2) -> create() -> each(function ($questionnaire) {
+            //dd($questionnaire);
+            factory(Question::class, 3) -> create(['questionnaire_id' => $questionnaire -> id]);
         });
 /*        factory(Questionnaire::class, 2) -> create() -> each(function($questionnaire) {
             $questionnaire -> questions() -> save(factory(Question::class) -> make());
