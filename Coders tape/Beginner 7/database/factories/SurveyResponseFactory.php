b99ac200bc5d522959acc146e29d1954b31329b0
EGA-SUPREMO <cxiesestro@gmail.com>
2020-05-29 13:50:13 -0400

    Add surveys factories & a survey seeder
    Signed-off-by: EGA-SUPREMO <cxiesestro@gmail.com>

--- Coders tape/Beginner 7/database/factories/SurveyResponseFactory.php
+++ Coders tape/Beginner 7/database/factories/SurveyResponseFactory.php
@@ -3,10 +3,15 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\SurveyResponses;
+use App\Survey;
+use App\Question;
+use App\Answer;
 use Faker\Generator as Faker;
 
 $factory->define(SurveyResponses::class, function (Faker $faker) {
     return [
-        //
+    	'survey_id' => factory(Survey::class),
+    	'question_id' => factory(Question::class),
+    	'answer_id' => factory(Answer::class),
     ];
 });
