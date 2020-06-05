dbe31987f3a5fe03e53e38057851ada8fb2976c1
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-04 20:42:59 -0400

    Define index in PaymentType controller
    Signed-off-by: EGA-SUPREMO <cxiesestro@gmail.com>

--- Coders tape/Advanced 6/app/Http/Controllers/PaymentTypeController.php
+++ Coders tape/Advanced 6/app/Http/Controllers/PaymentTypeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\PaymentType;
+
 use Illuminate\Http\Request;
 
 class PaymentTypeController extends Controller
 {
-    //
+	public function index()
+	{
+		$paymentTypes = PaymentType::all();
+
+		return view('paymentType.index', compact('paymentTypes'));
+	}
 }
