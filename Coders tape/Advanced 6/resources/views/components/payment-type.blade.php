3a9bb257e3ba9442cf84a5d427d9ea55b4b25f9c
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-09 10:41:24 -0400

    Define payment-type component

--- Coders tape/Advanced 6/resources/views/components/payment-type.blade.php
+++ Coders tape/Advanced 6/resources/views/components/payment-type.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+<div {{ $attributes -> merge(['class' => 'text-center']) }}>
+	<h1>{{ $title }}</h1>
+	<h2><strong>{{ $miniinfo }}</strong></h2>
+	<p>
+		For you
+	</p>
+	<br>
+	<p>{{ $info}}</p>
+	<ul>
+		@foreach($list('my custom item -'.$miniinfo) as $item)
+			<li>{{ $item }}</li>
+		@endforeach
+	</ul>
+	{{ $slot }}
+	<h3>END OF THIS COMPONENT</h3>
 </div>
