e18e7cccc623aa4f6db3375022d92401c1ee0daa
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-10 14:02:58 -0400

    Use eloquent scopes in costumer

--- Coders tape/Beginner 7/app/Costumer.php
+++ Coders tape/Beginner 7/app/Costumer.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Costumer extends Model
 {
     protected $guarded = [];
+
+    public function scopeByActivity($query, $isActive)
+    {
+    	return $query -> where('active', $isActive);
+    }
 }
