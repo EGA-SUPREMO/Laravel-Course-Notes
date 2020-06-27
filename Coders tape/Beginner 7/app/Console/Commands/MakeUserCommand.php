b88c027e3410ae8c6c0064e2bebfcc1aa7e8e2d9
Alejandro Garcia <cxiesestro@gmail.com>
2020-06-27 12:15:12 -0400

    Add command make:user (#35)
    * add useless output message

---
+++ Coders tape/Beginner 7/app/Console/Commands/MakeUserCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+
+use Illuminate\Console\Command;
+
+class MakeUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:user {name=Juan Morrales Feo} {email=yourock@here.com} {password=nevermind}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new user to the users table';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = User::create([
+            'name' => $this->argument('name'),
+            'email' => $this->argument('email'),
+            'password' => $this->argument('password'),
+        ]);
+
+        $this->info($user->name.' added successfully.');
+        $this->warn('This is a nonsense warning');
+        $this->error('Error, warning printed');
+        $this->error('Error, broken english');
+    }
+}
