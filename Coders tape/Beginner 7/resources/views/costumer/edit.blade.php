4816bd799c1f9a4346ca1dcfc4732754dafb9ba2
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-28 16:29:46 -0400

    add enctype to costumer forms for images

--- Coders tape/Beginner 7/resources/views/costumer/edit.blade.php
+++ Coders tape/Beginner 7/resources/views/costumer/edit.blade.php
@@ -1,6 +1,6 @@
 <h1>Edit a new costumer</h1>
 
-<form action="/costumers/{{ $costumer->id }}" method="POST">
+<form action="/costumers/{{ $costumer->id }}" method="POST" enctype="multipart/form-data">
 	@method('PATCH')
 
 	@include('costumer.form')
