654b3332a7ac22b0b63ac3f8a74634de4e07a30c
EGA-SUPREMO <cxiesestro@gmail.com>
2020-06-30 14:28:29 -0400

    add begin page to costumer views in a dirty way

--- Coders tape/Beginner 7/resources/views/begin-page.blade.php
+++ Coders tape/Beginner 7/resources/views/begin-page.blade.php
@@ -1,6 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="robots" content="noindex">
 
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
--- Coders tape/Beginner 7/resources/views/costumer/create.blade.php
+++ Coders tape/Beginner 7/resources/views/costumer/create.blade.php
@@ -1,3 +1,25 @@
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name') }}</title>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+<!-- Styles -->
+<!--     !!!Remove this in production -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+<!-- end of removing -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
 <h1>add a new costumer</h1>
 
 <form action="/costumers" method="POST" enctype="multipart/form-data">
@@ -5,3 +27,4 @@
 
 	<button type="submit">add costumer</button>
 </form>
+</body>
