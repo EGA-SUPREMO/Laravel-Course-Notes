336c25eadee9c5b1f4299de2c730221b547d74ae
Alejandro Garcia <cxiesestro@gmail.com>
2020-07-03 16:29:49 -0400

    Add roles (#50)
    * add role model with its migration
    * add pivot table for roles and users
    * add route for the practice
    * add relationship
    * add detach in practice file
    * add Timestamps
    * add more methods in web file

---
+++ Coders tape/Beginner 7/app/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    /**
+     * Turn off mass assignment protection
+     *
+     * @var array
+     */
+    protected $guarded = [];
+    
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
