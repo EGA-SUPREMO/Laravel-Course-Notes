5421d880a788fe80ccd9a4769ec7ccb193514f81
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-03 10:11:48 -0400

    add role model with its migration

---
+++ Coders tape/Beginner 7/app/Role.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    /**
+     * Turn off mass assignment protection
+     *
+     * @var array
+     */
+    protected $guarded = [];
+    
+}
