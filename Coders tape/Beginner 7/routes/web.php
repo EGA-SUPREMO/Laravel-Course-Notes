b840ba44e43217c07d5a1995e227bb95d9ffa1dd
Alejandro Garcia <cxiesestro@gmail.com>
2020-07-03 19:26:51 -0400

    Save who gave roles (#51)
    * add name to know who gave away that role
    
    * add pivot the relationship

--- Coders tape/Beginner 7/routes/web.php
+++ Coders tape/Beginner 7/routes/web.php
@@ -52,10 +52,13 @@ Route::get('/relationships', function()
 
 	$roles = Role::all();
 
-	$user->roles()->attach($roles);
+/*	$user->roles()->attach($roles);
 	$user->roles()->attach([1, 3, 5]);
 	$user->roles()->sync([1, 3, 5]);
-	$user->roles()->syncWithoutDetaching([1, 3, 5]);//most usefful for mee
+	$user->roles()->syncWithoutDetaching([1, 3, 5]);*///most usefful for mee
 	//$user->roles()->detach($roles);
-
+	dd($user->roles()->first()->pivot->name);
+	$user->roles()->syncWithoutDetaching([1 => [
+		'name' => 'Gustavo Fernandez Salas',
+	]]);
 });
