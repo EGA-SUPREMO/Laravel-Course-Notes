32613e3868b256910c9ee9e7be272ef659073f44
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-09 11:52:55 -0400

    add bookCanBeAdded test

---
+++ Coders tape/Test Driven Development/tests/Feature/BookReservationTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookReservationTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testBookCanBeAdded()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Good book',
+            'author' => 'Don Reveron',
+        ]);
+
+        $response->assertOk();
+        $response->assertCount(1,  Book::all());
+    }
+}
