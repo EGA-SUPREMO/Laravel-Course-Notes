b5b8693f9520e0ba77763faf26343cb4c55186a2
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-13 13:38:54 -0400

    add logged user access to questionnaire test

--- Coders tape/Beginner 7/tests/Feature/QuestionnaireTest.php
+++ Coders tape/Beginner 7/tests/Feature/QuestionnaireTest.php
@@ -5,16 +5,27 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+use App\User;
+
 class QuestionnaireTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
-     * /tests/Feature/ExampleTest.php
+     * 
      * @return void
      */
     public function testUnloggedUserGoesToLoginPageWhenAccessingToQuestionnaireIndex()
     {
         $response = $this->get('/questionnaires')
             ->assertRedirect('/login');
+    }
+    public function testLoggedUserAccessToQuestionnaireIndex()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $response = $this->get('/questionnaires');
+
+        $response->assertOk();
     }
 }
