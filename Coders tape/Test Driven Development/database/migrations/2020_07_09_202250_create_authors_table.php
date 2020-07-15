35ad2616d44091f457693a74224cee86a6cfbf37
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-15 19:27:34 -0400

    fix birth row

--- Coders tape/Test Driven Development/database/migrations/2020_07_09_202250_create_authors_table.php
+++ Coders tape/Test Driven Development/database/migrations/2020_07_09_202250_create_authors_table.php
@@ -16,7 +16,7 @@ class CreateAuthorsTable extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('birth');
+            $table->timestamp('birth');
             $table->timestamps();
         });
     }
