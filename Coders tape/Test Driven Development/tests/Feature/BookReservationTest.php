72248740af9d048abfb3c631e0405e946f8a2a2f
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-18 15:56:23 -0400

    rename test to book can be checkout checkin twice

--- Coders tape/Test Driven Development/tests/Feature/BookReservationTest.php
+++ Coders tape/Test Driven Development/tests/Feature/BookReservationTest.php
@@ -53,40 +53,54 @@ class BookReservationTest extends TestCase
         $this->assertEquals($book->id, $reservations->first()->book_id);
         $this->assertEquals(now(), $reservations->first()->check_in_at);
     }
-    public function test_book_can_be_checkout_checked_in_and_checkout_again()
+    public function test_book_can_be_checkout_checked_in_twice()
     {
         $book = factory(Book::class)->create();
         $user = factory(User::class)->create();
 
         $book->checkout($user);
         $reservations = Reservation::all();
-
+        $firstCheckoutDate = now();
         $this->assertCount(1, $reservations);
         $this->assertEquals($user->id, $reservations->first()->user_id);
         $this->assertEquals($book->id, $reservations->first()->book_id);
-        $this->assertEquals(now(), $reservations->first()->check_out_at);
+        $this->assertEquals($firstCheckoutDate, $reservations->first()->check_out_at);
         $this->assertNull($reservations->first()->check_in_at);
         ////////// the above should be reduced
 
         $book->checkin($user);
         $reservations = Reservation::all();
+        $firstCheckinDate = now();
 
         $this->assertCount(1, $reservations);
         $this->assertEquals($user->id, $reservations->first()->user_id);
         $this->assertEquals($book->id, $reservations->first()->book_id);
-        $this->assertEquals(now(), $reservations->first()->check_in_at);
+        $this->assertEquals($firstCheckinDate, $reservations->first()->check_in_at);
         ////////// the above should be reduced TOO
-        sleep(10);
+        sleep(5);
         $book->checkout($user);
         $reservations = Reservation::all();
-        //dd($reservations);
+        
         $this->assertCount(2, $reservations);
         $this->assertEquals($user->id, $reservations->find(2)->user_id);
         $this->assertEquals($book->id, $reservations->find(2)->book_id);
         $this->assertEquals(now(), $reservations->find(2)->check_out_at);
         $this->assertNull($reservations->find(2)->check_in_at);
         ////////// the above should be reduced
+        $book->checkin($user);
+        $reservations = Reservation::all();
 
+        $this->assertCount(2, $reservations);
+        $this->assertEquals($user->id, $reservations->find(2)->user_id);
+        $this->assertEquals($book->id, $reservations->find(2)->book_id);
+        $this->assertEquals(now(), $reservations->find(2)->check_in_at);
+        ////////// the above should be reduced TOO
+        $r = Reservation::where([
+            'user_id' => $user->id,
+        ]);
+        //dd($r);
+        $this->assertEquals($firstCheckoutDate, $reservations->first()->check_out_at);
+        $this->assertEquals($firstCheckinDate, $reservations->first()->check_in_at);
     }
     public function test_book_cannot_be_checked_in_without_being_check_out_first()
     {
