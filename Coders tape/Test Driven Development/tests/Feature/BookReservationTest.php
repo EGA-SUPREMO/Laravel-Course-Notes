b7b94043c065db9b5aa534506cb1bd18cdf8534e
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-18 13:13:18 -0400

    add book can be checkin test

--- Coders tape/Test Driven Development/tests/Feature/BookReservationTest.php
+++ Coders tape/Test Driven Development/tests/Feature/BookReservationTest.php
@@ -27,4 +27,27 @@ class BookReservationTest extends TestCase
         $this->assertEquals($book->id, $reservations->first()->book_id);
         $this->assertEquals(now(), $reservations->first()->check_out_at);
     }
+
+    public function test_book_can_be_checked_in()
+    {
+        $book = factory(Book::class)->create();
+        $user = factory(User::class)->create();
+
+        $book->checkout($user);
+        $reservations = Reservation::all();
+
+        $this->assertCount(1, $reservations);
+        $this->assertEquals($user->id, $reservations->first()->user_id);
+        $this->assertEquals($book->id, $reservations->first()->book_id);
+        $this->assertEquals(now(), $reservations->first()->check_out_at);
+        ////////// the above should be reduced
+
+        $book->checkin($user);
+        $reservations = Reservation::all();
+
+        $this->assertCount(1, $reservations);
+        $this->assertEquals($user->id, $reservations->first()->user_id);
+        $this->assertEquals($book->id, $reservations->first()->book_id);
+        $this->assertEquals(now(), $reservations->first()->check_in_at);
+    }
 }
