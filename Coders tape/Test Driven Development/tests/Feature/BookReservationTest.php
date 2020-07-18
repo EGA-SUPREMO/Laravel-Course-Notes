7cc2ab14802601f82390bdb6ce7773aa764d9e47
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-18 13:45:39 -0400

    add book can be checkout twice and other tests

--- Coders tape/Test Driven Development/tests/Feature/BookReservationTest.php
+++ Coders tape/Test Driven Development/tests/Feature/BookReservationTest.php
@@ -53,4 +53,49 @@ class BookReservationTest extends TestCase
         $this->assertEquals($book->id, $reservations->first()->book_id);
         $this->assertEquals(now(), $reservations->first()->check_in_at);
     }
+    public function test_book_can_be_checkout_checked_in_and_checkout_again()
+    {
+        $book = factory(Book::class)->create();
+        $user = factory(User::class)->create();
+
+        $book->checkout($user);
+        $reservations = Reservation::all();
+
+        $this->assertCount(1, $reservations);
+        $this->assertEquals($user->id, $reservations->first()->user_id);
+        $this->assertEquals($book->id, $reservations->first()->book_id);
+        $this->assertEquals(now(), $reservations->first()->check_out_at);
+        $this->assertNull($reservations->first()->check_in_at);
+        ////////// the above should be reduced
+
+        $book->checkin($user);
+        $reservations = Reservation::all();
+
+        $this->assertCount(1, $reservations);
+        $this->assertEquals($user->id, $reservations->first()->user_id);
+        $this->assertEquals($book->id, $reservations->first()->book_id);
+        $this->assertEquals(now(), $reservations->first()->check_in_at);
+        ////////// the above should be reduced TOO
+        sleep(10);
+        $book->checkout($user);
+        $reservations = Reservation::all();
+        //dd($reservations);
+        $this->assertCount(2, $reservations);
+        $this->assertEquals($user->id, $reservations->find(2)->user_id);
+        $this->assertEquals($book->id, $reservations->find(2)->book_id);
+        $this->assertEquals(now(), $reservations->find(2)->check_out_at);
+        $this->assertNull($reservations->find(2)->check_in_at);
+        ////////// the above should be reduced
+
+    }
+    public function test_book_cannot_be_checked_in_without_being_check_out_first()
+    {
+        $book = factory(Book::class)->create();
+        $user = factory(User::class)->create();
+
+        $book->checkin($user);
+        $reservations = Reservation::all();
+
+        $this->assertCount(0, $reservations);
+    }
 }
