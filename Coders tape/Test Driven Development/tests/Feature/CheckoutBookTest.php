c2b3ffd204b34e3bd26bc9a915df7431e7e210f1
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-19 16:49:40 -0400

    add CheckoutBookTest

---
+++ Coders tape/Test Driven Development/tests/Feature/CheckoutBookTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\User;
+use App\Book;
+use App\Reservation;
+
+class CheckoutBookTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_book_can_be_checkout_by_logged_user()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $book = factory(Book::class)->create();
+
+        $this->post('/checkout/'.$book->id);
+
+        $reservations = Reservation::all();
+
+        $this->assertCount(1, $reservations);
+        $this->assertEquals($user->id, $reservations->first()->user_id);
+        $this->assertEquals($book->id, $reservations->first()->book_id);
+        $this->assertEquals(now(), $reservations->first()->check_out_at);
+        $this->assertNull($reservations->first()->check_in_at);
+    }
+}
