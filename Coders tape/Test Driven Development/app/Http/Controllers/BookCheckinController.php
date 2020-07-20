d52867d8c8ca7419f906794004ff5da641d1f4e2
Alejandro Garcia <cxiesestro@gmail.com>
2020-07-20 10:19:13 -0400

    Book cannot be checked in while unlogged (#81)
    * add test_book_cannot_be_checkin_by_unlogged_user
    
    * add auth to checkin controller & fix test

--- Coders tape/Test Driven Development/app/Http/Controllers/BookCheckinController.php
+++ Coders tape/Test Driven Development/app/Http/Controllers/BookCheckinController.php
@@ -8,6 +8,11 @@ use App\Book;
 
 class BookCheckinController extends Controller
 {
+    function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(Book $book)
     {
         $book->checkin(auth()->user());
